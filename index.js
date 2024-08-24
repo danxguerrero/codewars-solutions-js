b7a9c3edc21f1a10e3639e7f5d9b322796b97068
danxguerrero <danxguerrero@yahoo.com>
2024-08-23 23:20:59 -0400

    Short Long Short solution

--- index.js
+++ index.js
@@ -21,4 +21,35 @@ function sumArray(array) {
     
     return array.reduce((sum, num) => sum + num, 0)
     
+}
+
+// 8 kyu Short Long Short
+
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+// ShortLongShort.solution("1", "22"); // returns "1221"
+// ShortLongShort.solution("22", "1"); // returns "1221"
+
+function solution(a, b){
+  let short
+  let long
+  
+  if (a.length > b.length) {
+    short = b
+    long = a
+  } else {
+    short = a
+    long = b
   }
+  
+  return short + long + short
+
+  // (a.length > b.length) ? b+a+b : a+b+a
+}
