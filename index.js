7b53b1c8b989ba2fb0370489191403f61959fb89
danxguerrero <danxguerrero@yahoo.com>
2024-08-24 00:11:35 -0400

    Testing 1-2-3 solution

--- index.js
+++ index.js
@@ -53,3 +53,26 @@ function solution(a, b){
 
   // (a.length > b.length) ? b+a+b : a+b+a
 }
+
+// 7 kyu Testing 1-2-3
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+var number=function(array){
+  let solution = []
+  for (let i = 0; i < array.length; i++) {
+    solution.push(`${i+1}: ${array[i]}`)
+  }
+  return solution
+}
+
