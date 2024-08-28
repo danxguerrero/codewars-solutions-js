c10312e6d040f3c4ad3620638c5e133faaebf83d
danxguerrero <danxguerrero@yahoo.com>
2024-08-27 22:29:20 -0400

    Two Sum solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 8 kyu Sum without highest and lowest number
+// 8 kyu - Sum without highest and lowest number
 
 // Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
 // The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
@@ -23,7 +23,7 @@ function sumArray(array) {
     
 }
 
-// 8 kyu Short Long Short
+// 8 kyu - Short Long Short
 
 // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
@@ -54,7 +54,7 @@ function solution(a, b){
   // (a.length > b.length) ? b+a+b : a+b+a
 }
 
-// 7 kyu Testing 1-2-3
+// 7 kyu - Testing 1-2-3
 
 // Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 
@@ -76,7 +76,7 @@ var number=function(array){
   return solution
 }
 
-// 8 kyu Grader
+// 8 kyu - Grader
 
 
 // Create a function that takes a number as an argument and returns a grade based on that number.
@@ -105,8 +105,7 @@ function grader(score) {
 }
 
 
-// 7 kyu
-// Mumbling
+// 7 kyu - Mumbling
 // This time no story, no theory. The examples below show you how to write function accum:
 
 // Examples:
@@ -119,4 +118,34 @@ function accum(s) {
     .split('') // Split input into array of characters
     .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index)) // Map each character in array to an array of 1 upper case letter and then repeat character in lower case index times
     .join('-'); // Join each part of array with a hyphen
+}
+
+//6 kyu - Two Sum
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+// Based on: https://leetcode.com/problems/two-sum/
+
+// twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+// twoSum([3, 2, 4], 6) // returns [1, 2] or [2, 1]
+
+function twoSum(numbers, target) {
+  // empty object to store tested numbers
+  testedNums = {}
+  
+  for (i = 0; i < numbers.length; i++) {
+    // Subtract target from number at numbers index i
+    const compliment = target - numbers[i]
+    // Check to see if compliment is in hashmap
+    if (testedNums[compliment] !== undefined) {
+      // if it exists, return an array with the compliments index and i
+      return [testedNums[compliment], i]
+    }
+    
+    // If compliment is not in testedNums, add numbers[i] and i to testedNums
+    testedNums[numbers[i]] = i
+  }
 }
