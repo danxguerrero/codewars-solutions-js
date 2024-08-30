39c09284e77134b0c890144dea4a9a88d37bfe4a
danxguerrero <danxguerrero@yahoo.com>
2024-08-29 22:01:25 -0400

    Moving Zeros To The End Solution

--- index.js
+++ index.js
@@ -182,3 +182,25 @@ function alphabetPosition(text) {
   // Join each letter in the array
   return positions.join(" ")
 }
+
+// 5 kyu - Moving Zeros To The End
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(arr) {
+  let zeroCount = 0
+  let result = []
+  
+  for (i=0;i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      result.push(arr[i])
+    } else {
+      zeroCount++
+    }
+  }
+  for (i=1; i <= zeroCount; i++) {
+    result.push(0)
+  }
+  return result
+}
