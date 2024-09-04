8707ff77fd3c68af167aba2d5959b2f81122f728
danxguerrero <danxguerrero@yahoo.com>
2024-09-03 22:50:43 -0400

    New season, new league Solution

--- index.js
+++ index.js
@@ -310,3 +310,40 @@ function add(a, b) {
   
   return result.join('') // Join the array back into a string
 }
+
+
+// 7 kyu - New season, new league
+// The new football league season is coming and the Football Association need some help resetting the league standings. Normally the initial league standing is done in alphabetical order (from A to Z) but this year the FA have decided to freshen it up.
+
+// It has been decided that team who finished first last season will begin the next season in first place. Regardless of what letter their club begins with. e.g. if Manchester City were in first place last year, they will begin the season in position one. All other teams should be in alphabetical order.
+
+// The teams will be fed in as an object ({}). The key will be will be their position from last season and the value is the club's name e.g. Arsenal.
+
+// The output should be an object ({}) with the key as the club's starting position for the new season and the value should be club's name e.g. Arsenal.
+
+// For example. If in the previous season the standings were:
+
+// 1:'Leeds United' 2:'Liverpool' 3:'Manchester City' 4:'Coventry' 5:'Arsenal'
+
+// Then the new season standings should
+
+// 1:'Leeds United' (first last season) 2:'Arsenal' (alphabetical) 3:'Coventry' (alphabetical) 4:'Liverpool' (alphabetical) 5:'Manchester City' (alphabetical)
+
+function premierLeagueStandings(teamStandings) {
+  const sorted = []
+  
+  // Add teamStandings[2] and up to an array
+  for (i=1; i < Object.keys(teamStandings).length; i++) {
+    sorted.push(teamStandings[i + 1])
+  }
+  
+  // Sort that array
+  sorted.sort()
+ 
+  // set teamStandings[2] to array[0] and so on
+  for (i=0; i < sorted.length; i++) {
+    teamStandings[i+2] = sorted[i]
+  }
+  
+  return teamStandings
+}
