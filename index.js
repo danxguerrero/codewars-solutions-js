fe35f1f045e500ac980d1fe0d8d1f13442210b54
danxguerrero <danxguerrero@yahoo.com>
2024-09-08 20:34:07 -0400

    A Chain adding function solution

--- index.js
+++ index.js
@@ -493,3 +493,38 @@ function dividedBy(rightOp) {
   }
 }
 
+// 5 kyu - A Chain adding function
+// We want to create a function that will add numbers together when called in succession.
+
+// add(1)(2); // == 3
+// We also want to be able to continue to add numbers to our chain.
+
+// add(1)(2)(3); // == 6
+// add(1)(2)(3)(4); //  == 10
+// add(1)(2)(3)(4)(5); // == 15
+// and so on.
+
+// A single call should be equal to the number passed in.
+
+// add(1); // == 1
+// We should be able to store the returned values and reuse them.
+
+// var addTwo = add(2);
+// addTwo; // == 2
+// addTwo + 5; // == 7
+// addTwo(3); // == 5
+// addTwo(3)(5); // == 10
+// We can assume any number being passed in will be valid whole number.
+
+function add(n) {
+  // We store the current sum in a closure
+  const chain = (x) => add(n + x); // Return a new `add` function with the updated sum
+
+  // Overloading the `valueOf` method to return the current sum
+  chain.valueOf = () => n;
+
+  // Also overload `toString` to handle string concatenation cases
+  chain.toString = () => n.toString();
+
+  return chain;
+}
