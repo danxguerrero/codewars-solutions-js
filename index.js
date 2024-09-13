c5ccdd4274158d33503f7415441a381b8c3c31d3
danxguerrero <danxguerrero@yahoo.com>
2024-09-12 23:21:42 -0400

    Strings Mix Solution

--- index.js
+++ index.js
@@ -570,3 +570,100 @@ function solution(list){
   // Join the result array with commas and return
   return result.join(',')
 }
+
+//4 kyu - Strings Mix
+// Given two strings s1 and s2, we want to visualize how different the two strings are. We will only take into account the lowercase letters (a to z). First let us count the frequency of each lowercase letters in s1 and s2.
+
+// s1 = "A aaaa bb c"
+
+// s2 = "& aaa bbb c d"
+
+// s1 has 4 'a', 2 'b', 1 'c'
+
+// s2 has 3 'a', 3 'b', 1 'c', 1 'd'
+
+// So the maximum for 'a' in s1 and s2 is 4 from s1; the maximum for 'b' is 3 from s2. In the following we will not consider letters when the maximum of their occurrences is less than or equal to 1.
+
+// We can resume the differences between s1 and s2 in the following string: "1:aaaa/2:bbb" where 1 in 1:aaaa stands for string s1 and aaaa because the maximum for a is 4. In the same manner 2:bbb stands for string s2 and bbb because the maximum for b is 3.
+
+// The task is to produce a string in which each lowercase letters of s1 or s2 appears as many times as its maximum if this maximum is strictly greater than 1; these letters will be prefixed by the number of the string where they appear with their maximum value and :. If the maximum is in s1 as well as in s2 the prefix is =:.
+
+// In the result, substrings (a substring is for example 2:nnnnn or 1:hhh; it contains the prefix) will be in decreasing order of their length and when they have the same length sorted in ascending lexicographic order (letters and digits - more precisely sorted by codepoint); the different groups will be separated by '/'. See examples and "Example Tests".
+
+// Hopefully other examples can make this clearer.
+
+// s1 = "my&friend&Paul has heavy hats! &"
+// s2 = "my friend John has many many friends &"
+// mix(s1, s2) --> "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+// s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+// s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+// mix(s1, s2) --> "1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
+
+// s1="Are the kids at home? aaaaa fffff"
+// s2="Yes they are here! aaaaa fffff"
+// mix(s1, s2) --> "=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"
+// Note for Swift, R, PowerShell
+// The prefix =: is replaced by E:
+
+// s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+// s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+// mix(s1, s2) --> "1:mmmmmm/E:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/E:ee/E:ss"
+
+function mix(s1, s2) {
+  const letterCount1 = letterCount(s1);
+  const letterCount2 = letterCount(s2);
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  
+  const result = [];
+  
+  // Loop through each letter in the alphabet
+  for (let i = 0; i < alphabet.length; i++) {
+    const char = alphabet[i];
+    // Get the count of the current letter in both strings
+    const count1 = letterCount1[char] || 0;
+    const count2 = letterCount2[char] || 0;
+    
+    // If the count is greater than 1, add it to the result
+    if (count1 > 1 || count2 > 1) {
+      // If the count in s1 is greater than in s2, add it as 1:
+      if (count1 > count2) {
+        result.push(`1:${char.repeat(count1)}`);
+      } else if (count2 > count1) {
+        result.push(`2:${char.repeat(count2)}`);
+      } else if (count1 === count2) {
+        result.push(`=:${char.repeat(count1)}`);
+      }
+    }
+  }
+  
+  // Sort by length first, then by the prefix order ("1:", "2:", "=:"), and finally lexicographically
+  result.sort((a, b) => {
+    // Sort by length (longer comes first)
+    if (b.length !== a.length) {
+      return b.length - a.length;
+    }
+    // Sort by prefix (1: < 2: < =:)
+    const prefixOrder = { '1': 1, '2': 2, '=': 3 };
+    if (prefixOrder[a[0]] !== prefixOrder[b[0]]) {
+      return prefixOrder[a[0]] - prefixOrder[b[0]];
+    }
+    // Sort lexicographically if same prefix and length
+    return a.localeCompare(b);
+  });
+  
+  return result.join('/');
+}
+
+function letterCount(str) {
+  let letters = {}; // Fix declaration
+  
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char >= 'a' && char <= 'z') { // Only count lowercase letters
+      letters[char] = (letters[char] || 0) + 1;
+    }
+  }
+  
+  return letters;
+}
