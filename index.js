5ca48e7afe075daea0feb99903a6293602c5a480
danxguerrero <danxguerrero@yahoo.com>
2024-09-16 22:57:44 -0400

    The old switcheroo Solution

--- index.js
+++ index.js
@@ -678,3 +678,28 @@ function letterCount(str) {
 function sortGiftCode(code) {
   return code.split('').sort().join('');
 }
+
+// 7 kyu - The old switcheroo
+// Write a function
+
+// vowel2index(str)
+// that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+// E.g:
+
+// vowel2index('this is my string') == 'th3s 6s my str15ng'
+// vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+// vowel2index('') == ''
+// Your function should be case insensitive to the vowels.
+
+function vowel2index(str) {
+  const vowels = ['a','e','i','o','u']
+  const characters = str.split('')
+  
+  for (i=0; i<characters.length;i++) {
+     if (vowels.includes(characters[i].toLowerCase())) {
+       characters[i] = i+1
+     }
+  }
+  
+  return characters.join('')
+}
